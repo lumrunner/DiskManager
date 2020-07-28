4a1859f1c38bb18d299d99daf33cf44af4593536
lumrunner <43517700+lumrunner@users.noreply.github.com>
2020-07-28 14:19:21 -0400

    loop update

--- copyData.py
+++ copyData.py
@@ -3,9 +3,9 @@ BACKUP_DRIVE = r'\\.\PhysicalDrive2'
 DATA_DRIVE = r'\\.\PhysicalDrive3'
 
 count = 0
-while count < 10:
-	with open(DATA_DRIVE, 'rb+') as dataDrive:
-		data = dataDrive.read(SECTOR_SIZE)
-		with open(BACKUP_DRIVE, 'rb+') as backup:
+with open(DATA_DRIVE, 'rb+') as dataDrive:
+	with open(BACKUP_DRIVE, 'rb+') as backup:
+		for count in range(0, 100000):
+			backup.seek(SECTOR_SIZE * count)
+			data = dataDrive.read(SECTOR_SIZE)
 			backup.write(data)
-	count+=1
